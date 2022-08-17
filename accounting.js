3ad05b85ecd76245b3a5c8c579266ad8a380fdcb
AhmadyZahran <ahmadyousefzahran@gmail.com>
2022-08-18 00:31:27 +0300

    upload card

--- accounting.js
+++ accounting.js
@@ -1,9 +1,9 @@
-function Employee(employeeId, fullName, department, level, imageUrl) {
-    this.employeeId = employeeId
-    this.fullName = fullName
-    this.department = department
-    this.level = level
-    this.imageUrl = imageUrl
+function Employee(imageUrl, fullName, employeeId, department, level) {
+    this.employeeId = employeeId;
+    this.fullName = fullName;
+    this.department = department;
+    this.level = level;
+    this.imageUrl = imageUrl;
     this.salary = () => {
         if (this.level.toLowerCase() === 'junior') {
             return Math.trunc(Math.random() * 500 + 500);
@@ -21,47 +21,57 @@ function Employee(employeeId, fullName, department, level, imageUrl) {
 // // call the parent 
 let container = document.getElementById('container');
 //create child
-let tableOfEmployees = document.createElement('table');
 
-container.appendChild(tableOfEmployees);
 
-let row = document.createElement("tr");
-
-const data = ["employeeId", "fullName", "department", "level", "imageUrl", "salary"];
-
-for (let i = 0; i < data.length; i++) {
-
-    let th = document.createElement("th")
-    row.appendChild(th)
-    th.innerText = data[i];
-
-}
-
-tableOfEmployees.appendChild(row);
-
+const allEmployees = [];
+let employee;
 
 let form = document.getElementById('myForm');
-
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    let row = document.createElement("tr");
     let employeeId = document.getElementById('employeeId').value;
     let fullNAme = document.getElementById('fullName').value;
     let department = document.getElementById('department').value;
     let level = document.getElementById('LevelName').value;
     let imageUrl = document.getElementById('imageUrl').value;
-
-    const employee = new Employee(employeeId, fullNAme, department, level, imageUrl);
-
-    for (info in employee) {
-        let td = document.createElement("td");
-        if (info === 'salary') {
-            td.innerHTML = employee.salary();
-        } else {
-            td.innerHTML = employee[info];
-        }
-        row.appendChild(td);
+    employee = new Employee(imageUrl, fullNAme, employeeId, department, level);
+    employee['salary'] = employee.salary();
+    if (!JSON.parse(localStorage.getItem('employees'))) {
+        localStorage.setItem('employees', JSON.stringify([employee]));
+    } else {
+        let newArray = JSON.parse(localStorage.getItem('employees'));
+        console.log(newArray);
+        newArray.push(employee);
+        localStorage.setItem('employees', JSON.stringify(newArray));
 
     }
-    tableOfEmployees.appendChild(row);
-})
+    form.reset();
+});
+
+
+const section = document.querySelector('#allEmployees');
+if (JSON.parse(localStorage.getItem('employees'))) {
+    const informationArray = JSON.parse(localStorage.getItem('employees'));
+
+    informationArray.forEach(employee => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+                    <img src='${employee.imageUrl}' alt="John" style="width:100%">
+                    <h1> ${employee.fullName}  ${employee.employeeId} </h1 >
+                    <p>${employee.department}  ${employee.level}</p>
+                    <p>${employee.salary}</p>                
+                            <div style="margin: 24px 0;">
+                            <a href="#"><i class="fa fa-dribbble"></i></a> 
+                            <a href="#"><i class="fa fa-twitter"></i></a>  
+                            <a href="#"><i class="fa fa-linkedin"></i></a>  
+                            <a href="#"><i class="fa fa-facebook"></i></a> 
+                            </div>
+                    <p><button>Contact</button></p>
+    
+        `;
+        section.appendChild(card);
+    })
+}
+
+
